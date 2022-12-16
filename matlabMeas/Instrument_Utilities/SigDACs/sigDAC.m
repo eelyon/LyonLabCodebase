1b73e9f30a9e2eaca28b6a60ac049675e52b4873
Mierzzz <118632635+Mierzzz@users.noreply.github.com>
2022-12-16 08:09:43 -0500

    Changed to class based DAC

---
+++ matlabMeas/Instrument_Utilities/SigDACs/sigDAC.m
@@ -0,0 +1,46 @@
+classdef sigDAC
+    %SIGDAC Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        numChannels {mustBeNumeric}
+        comPort  
+        channelVoltages = []
+        client
+    end
+    
+    methods
+        function sigDAC = sigDAC(comPort, numChannels)
+            
+            sigDAC.comPort = comPort;
+            sigDAC.client = serial_Connect(comPort);
+
+            sigDAC.numChannels = numChannels;
+            sigDAC.channelVoltages = zeros(1,numChannels+1);
+            sigDAC.channelVoltages(1) = 'sigDAC Channel Voltages (V)';
+            for i = 1:numChannels
+                sigDAC.channelVoltages(i+1) = sigDACQueryVoltage(sigDAC,i);
+            end
+
+        end
+        
+        function voltage = sigDACQueryVoltage(sigDAC,channel)
+                fprintf(sigDAC.client,['CH ' num2str(channel)]);
+                voltage = str2double(query(sigDAC.client,'VOLT?'));
+        end
+
+        function sigDACSetVoltage(sigDAC,channel,voltage)
+                fprintf(sigDAC.client,['CH ' num2str(channel)]);
+                fprintf(sigDAC.client,['VOLT ' num2str(voltage)]);
+        end
+
+        function  voltageArr = sigDACGetConfig(sigDAC)
+            for channel = 1:sigDAC.numChannels
+                sigDAC.channelVoltages(channel+1) = sigDACQueryVoltage(sigDAC,channel);
+            end
+            voltageArr = sigDAC.channelVoltages;
+        end
+
+    end
+end
+
