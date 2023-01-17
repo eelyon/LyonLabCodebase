ca1872cf4e4ad5c6911e4ba2b2d74e62e97bcacc
Tiffany <tliu2@princeton.edu>
2023-01-17 15:19:21 -0500

    added thermometer calibration

--- matlabMeas/Thermometry/getChebCoefficients.m
+++ matlabMeas/Thermometry/getChebCoefficients.m
@@ -1,6 +1,8 @@
 function [coefficients] = getChebCoefficients(thermometerType)
     if strcmp(thermometerType,'X117656')
         coefficients = [5.649284,-6.496949,2.795808,-0.969290,.271303,-0.056705,.005340,.002012,-.001516,.00707];
+    elseif strcmp(thermometerType,'X189328') % 1.4K to 14K
+        coefficients = [5.571487,-6.388558,2.750689,-0.955339,.270645,-0.059402,.007658,.000646,-0.001037];
     else
         disp('Your thermometer calibration does not exist!');
     end
