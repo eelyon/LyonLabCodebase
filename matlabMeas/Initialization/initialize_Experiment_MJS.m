2f21bd3e813da52ca055aef67e5d654a046ce24b
Matt <fizixboi@gmail.com>
2023-01-29 16:29:57 -0500

    Create initialize_Experiment_MJS.m

---
+++ matlabMeas/Initialization/initialize_Experiment_MJS.m
@@ -0,0 +1,19 @@
+%% Script used to initialize an experiment.
+
+port = 1234;
+
+% SR830_Address = '172.29.117.106';
+% SR830 = SR830(port,SR830_Address);
+% 
+% DMM_Address = '172.29.117.107';
+% Thermometer = TCPIP_Connect(DMM_Address,port);
+% 
+% AWG_Address = '172.29.117.108';
+% AWG = Agilent33220A(port,AWG_Address);
+
+% SR830GUI = SR830_GUI;
+SR830SWEEPGUI = sweepSR830GUI;
+
+SR830SWEEPGUI.SR830ReadEditField.Value = 'SR830';
+SR830SWEEPGUI.numRepeatsEditField.Value = 10;
+%DCMap;
