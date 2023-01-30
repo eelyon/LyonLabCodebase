1703255fd9d5f4eb96967477616c85e35a9a7464
Matt <fizixboi@gmail.com>
2023-01-30 18:05:07 -0500

    created setVoltagesDAC

--- matlabMeas/Initialization/initialize_Experiment_MMF.m
+++ matlabMeas/Initialization/initialize_Experiment_MMF.m
@@ -8,17 +8,17 @@ port = 1234;
 % DMM_Address = '172.29.117.107';
 % Thermometer = TCPIP_Connect(DMM_Address,port);
 
-% sigDACPort = 'COM5';
-% DAC = sigDAC(sigDACPort,8);
-% 
+sigDACPort = 'COM5';
+DAC = sigDAC(sigDACPort,8);
+
 % AWG_Address = '172.29.117.108';
 % AWG = Agilent33220A(port,AWG_Address);
 
 % DACGUI = sigDACGUI;
 % SR830GUI = SR830_GUI;
-SR830SWEEPGUI = sweepSR830GUI;
-
-SR830SWEEPGUI.SR830ReadEditField.Value = 'SR830';
-SR830SWEEPGUI.SweepDeviceEditField.Value = 'DAC';
-SR830SWEEPGUI.numRepeatsEditField.Value = 10;
-DCMap;
+% SR830SWEEPGUI = sweepSR830GUI;
+% 
+% SR830SWEEPGUI.SR830ReadEditField.Value = 'SR830';
+% SR830SWEEPGUI.SweepDeviceEditField.Value = 'DAC';
+% SR830SWEEPGUI.numRepeatsEditField.Value = 10;
+% DCMap;
