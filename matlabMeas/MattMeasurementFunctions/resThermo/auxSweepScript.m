a33887d2649bc6be8dde55526eb4c8bddf5100df
matt <fizixboi@gmail.com>
2023-01-31 14:50:31 -0500

    converting aux connections to DAC connections

--- matlabMeas/MattMeasurementFunctions/resThermo/auxSweepScript.m
+++ matlabMeas/MattMeasurementFunctions/resThermo/auxSweepScript.m
@@ -8,8 +8,8 @@ deltaParam = 0.05;
 timeBetweenPoints = 0.3;
 repeat = 10;
 readSR830 = {SR830};
-device = SR830;
-ports = {'1'};
+device = DAC;
+ports = {3};
 doBackAndForth = 1;
 
 sweep1DMeasSR830(sweepType,start,stop,deltaParam,timeBetweenPoints,repeat,readSR830,device,ports,doBackAndForth);
--- matlabMeas/MattMeasurementFunctions/resThermo/freqAuxSweepScript.m
+++ matlabMeas/MattMeasurementFunctions/resThermo/freqAuxSweepScript.m
@@ -8,7 +8,7 @@ deltaParams = {1000, 0.1};
 timeBetweenPoints = 0.5;
 repeat = 10;
 readSR830 = SR830;
-devices = {SR830,SR830};
-portss = {{'Freq'}, {'1'}};
+devices = {SR830,DAC};
+portss = {{'Freq'}, {3}};
 
 sweep2DMeasSR830_Func(sweepTypes, starts, stops, deltaParams, devices, portss, timeBetweenPoints,repeat,readSR830);
