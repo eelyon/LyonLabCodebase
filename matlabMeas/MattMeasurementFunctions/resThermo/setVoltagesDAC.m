380a833ba4f9d0f842e01f3f5787a41874334ec8
matt <fizixboi@gmail.com>
2023-03-13 19:39:01 -0400

    finished measuring 10 march 2023

--- matlabMeas/MattMeasurementFunctions/resThermo/setVoltagesDAC.m
+++ matlabMeas/MattMeasurementFunctions/resThermo/setVoltagesDAC.m
@@ -1,21 +1,33 @@
 %% Function to bulk set voltages on the DAC
 
-voltsEmitt = [-8,...  %DAC 1
-        3,...   %DAC 2
-        2,...   %DAC 3
-        2,...   %DAC 4
-        2,...   %DAC 5
+channels = [1, 2, 3, 4, 5, 6, 7, 8];
+numSteps = 100;
+
+voltsEmitt = [-4,...  %DAC 1
+        1.5,...   %DAC 2
+        1,...   %DAC 3
+        1,...   %DAC 4
+        1,...   %DAC 5
         0,...   %DAC 6
-        1,...   %DAC 7
+        0.25,...   %DAC 7
         0];     %DAC 8
 
 voltsKeep = [0,...  %DAC 1
-        3,...   %DAC 2
+        1.5,...   %DAC 2
+        1,...   %DAC 3
+        1,...   %DAC 4
+        1,...   %DAC 5
+        0,...   %DAC 6
+        -1,...   %DAC 7
+        0];     %DAC 8
+
+voltsQuo = [0,...  %DAC 1
+        0.5,...   %DAC 2
         0,...   %DAC 3
         0,...   %DAC 4
         0,...   %DAC 5
         0,...   %DAC 6
-        -1,...   %DAC 7
+        0,...   %DAC 7
         0];     %DAC 8
 
 voltsGnd = [0,...  %DAC 1
@@ -27,8 +39,14 @@ voltsGnd = [0,...  %DAC 1
         0,...   %DAC 7
         0];     %DAC 8
 
-voltUse = voltsKeep;
+%sigDACRampVoltage(DAC,channels,voltsEmitt,numSteps);
+%sigDACRampVoltage(DAC,channels,voltsKeep,numSteps);
+sigDACRampVoltage(DAC,channels,voltsQuo,numSteps);
 
+%sigDACRampVoltage(DAC,channels,voltsGnd,numSteps);
+
+%{
 for i = 1:length(voltUse)
     setVal(DAC,i,voltUse(i));
 end
+%}
--- matlabMeas/MattMeasurementFunctions/resThermo/sweepTMToGND.m
+++ matlabMeas/MattMeasurementFunctions/resThermo/sweepTMToGND.m
@@ -1,4 +1,6 @@
 guardPort = 2;
+steps = 20;
+stepSize = vguInit/steps;
 vguInit = sigDACQueryVoltage(DAC, guardPort);
 
-sweep1DMeasSR830({'TM'},vguInit,0,vguInit/10,1,10,{SR830},DAC,{2},0)
+sweep1DMeasSR830({'TM'},vguInit,0,stepSize,1,10,{SR830},DAC,{2},0);
