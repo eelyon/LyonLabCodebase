3c9202e515db912c41b144e8dc5b17c5431733f0
matt <fizixboi@gmail.com>
2023-03-19 19:01:06 -0400

    finished cooldown

--- matlabMeas/MattMeasurementFunctions/resThermo/setVoltagesDAC.m
+++ matlabMeas/MattMeasurementFunctions/resThermo/setVoltagesDAC.m
@@ -4,12 +4,12 @@ channels = [1, 2, 3, 4, 5, 6, 7, 8];
 numSteps = 100;
 
 voltsEmitt = [-4,...  %DAC 1
-        -0.25,...   %DAC 2
+        -0.2,...   %DAC 2
         0,...   %DAC 3
-        -1,...   %DAC 4
-        -1,...   %DAC 5
+        -0.5,...   %DAC 4
+        -0.5,...   %DAC 5
         0,...   %DAC 6
-        -1.5,...   %DAC 7
+        -1,...   %DAC 7
         0];     %DAC 8
 
 voltsGnd = [0,...  %DAC 1
