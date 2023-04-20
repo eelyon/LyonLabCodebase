b59063966d6c7fb5fda284c0dcb439c263e6e81b
tiffliu9 <tliu2@princeton.edu>
2023-04-20 14:30:33 -0400

    updated criticalElectrondensity.m

--- matlabMeas/QuickCalc/criticalElectronDensity.m
+++ matlabMeas/QuickCalc/criticalElectronDensity.m
@@ -1,21 +1,29 @@
 function [density] = criticalElectronDensity( thickness, varargin )
-  % calculates the critical density of electrons
-  % INPUTS: thickness = string, if 'thick' will calculate density for thick
-  % He film, anything else will do thin film; varargin = optional He film
-  % thickness in meters parameter if calculating for thin film
+  % calculates the critical density of electrons per cm^2
+  % INPUTS: thickness = string, 
+      % if 'thick', no other parameter
+      % if 'thin', input He film thickness in meters
+      % if 'fraction', input kmin in meters
+
   e = 1.609*10^-19;
-  rho = 145; %kg/m^3
-  sig = 3.34e-4; %N/m 
-  g = 9.8;  %cm/s
+  rho = 145;       % kg/m^3
+  sig = 3.34e-4;   % N/m 
+  g = 9.8;         % m/s
   ehe = 1.057;
-  e0 = 8.85e-12; % in m 
+  e0 = 8.85e-12;   % m 
+  cm = 1e-4;
 
   if strcmp(thickness,'thick')        % for thick film
-    density = 1e-4*((4*rho*g*sig)^(1/4)/(e^2/(ehe*e0))^(1/2));  % per cm^2
-  else                                % for thin film
+    density = cm*((4*rho*g*sig)^(1/4)/(e^2/(ehe*e0))^(1/2));  
+  
+  elseif strcmp(thickness,'thin')     % for thin film
     beta = 3.25e-22; % J, typical for metallic substrate 
     d = varargin{1};
-    density = 1e-4*(4*sig*(rho*g+((3*beta)/d^4)))^(1/4)/(e^2/(ehe*e0))^(1/2);  % per cm^2
-  end    
+    density = cm*(4*sig*(rho*g+((3*beta)/d^4)))^(1/4)/(e^2/(ehe*e0))^(1/2); 
   
+  elseif strcmp(thickness,'fraction') % for fractionated He films
+    kmin = varargin{1};
+    density = cm*sqrt((sig*kmin)/(e^2/(2*e0*ehe)));     
+  end    
+
 end
