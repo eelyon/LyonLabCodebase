eec8dffd0b547fdddb32d88b786982824373eaa9
tiffliu9 <tliu2@princeton.edu>
2023-07-03 20:20:01 -0400

    quick edit

--- matlabMeas/QuickCalc/electronDensityST.m
+++ matlabMeas/QuickCalc/electronDensityST.m
@@ -1,7 +1,7 @@
 function [ n ] = electronDensityST( Vpinch,d )
  % calculates the electron density given pinch off voltage 
  % INPUTs: Vpinch = negative pinch off voltage from STM sweep, 
- % d = ST channel height in m
+ % d = thickness of He on top of ST bottom metal height in m
  e = 1.609*10^-19;
  ehe = 1.057;
  e0 = 8.85e-12; 
