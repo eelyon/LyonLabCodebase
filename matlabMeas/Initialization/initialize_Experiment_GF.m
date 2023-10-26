2462011a09475d568095fc80ed955a7e00ca55aa
gordianf1 <gordian.fuchs@gmail.com>
2023-10-26 14:51:16 -0400

    Update initialize_Experiment_GF.m

--- matlabMeas/Initialization/initialize_Experiment_GF.m
+++ matlabMeas/Initialization/initialize_Experiment_GF.m
@@ -2,7 +2,7 @@
 
 port = 1234;
 
-DMM_Address = '172.29.117.107';
+DMM_Address = '172.29.117.107'; % This is the number of the ethernet card (??) at the back of the intstrument
 
 % Thermometer
 Thermometer = TCPIP_Connect(DMM_Address,port);
