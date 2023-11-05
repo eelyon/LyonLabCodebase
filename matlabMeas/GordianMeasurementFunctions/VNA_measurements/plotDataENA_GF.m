d0810fecfae9cfdd664c65d4fffbd1ed5eb9180c
mayer <118573753+tiffliu9@users.noreply.github.com>
2023-11-05 16:53:52 -0500

    initialize vna

--- matlabMeas/GordianMeasurementFunctions/VNA_measurements/plotDataENA_GF.m
+++ matlabMeas/GordianMeasurementFunctions/VNA_measurements/plotDataENA_GF.m
@@ -1,13 +1,13 @@
 % Set freq range
 E5071SetPower(ENA,0); % in dBm
 E5071SetStartFreq(ENA,1000); % in MHz
-E5071SetStopFreq(ENA,7000); % in MHz
+E5071SetStopFreq(ENA,3000); % in MHz
 
 % Set single sweep
 fprintf(ENA,':INIT1'); % Set trigger value - for continuous set: ':INIT:CONT ON'
 fprintf(ENA,':TRIG:SOUR BUS'); % Set trigger source to "Bus Trigger"
 fprintf(ENA,':TRIG:SING'); % Trigger ENA to start sweep cycle
-query(ENA,'*OPC?') % Execute *OPC? command and wait until command return 1
+query(ENA,'*OPC?'); % Execute *OPC? command and wait until command return 1
 
 % Get mag (log) and phase (deg) data
 tag = 'freqSweep';
@@ -19,7 +19,7 @@ subPlotFigure = figure(getNextMATLABFigNum());
 
 subplot(1,2,1);
 freqvsmag = plotData(fdata,mag,'xLabel',"Frequency (GHz)",'yLabel',strcat(measType," (dB)"),'color',"b.",'subPlot',1);
-subplot(1,2,2)
+subplot(1,2,2);
 freqvsphase = plotData(fdata,phase,'xLabel',"Frequency (GHz)",'yLabel',"\phi (^{\circ})",'color',"r.",'subPlot',1);
 
 % plotHandles = {freqvsmag,freqvsphase};
--- matlabMeas/GordianMeasurementFunctions/VNA_measurements/setupENA_GF.m
+++ matlabMeas/GordianMeasurementFunctions/VNA_measurements/setupENA_GF.m
@@ -1,21 +1,16 @@
-%% Initialise ENA
-%port = 5025;
-%ipAddress = '172.29.117.72';
-%ENA = TCPIP_Connect(ipAddress,port); % tcpclient is new version
-%ENA.InputBufferSize = 32000;
+% fopen(ENA) % open client connection
+% fclose(ENA) % close client connection, or: clear ENA
+% query(ENA,'*IDN?') % check connection to ENA
 
 %% Set up ENA
 % fprintf(ENA,':SYST:KLOC:KBD OFF'); % Lock front panel and keyboard
 % fprintf(ENA,':SYST:KLOC:MOUS OFF'); % Lock mouse and touch screen
+
 E5071SetAvg(ENA,'Off'); % Turn off averaging
 E5071SetMeas(ENA,1,'S21'); % S11, S12, S21, S22
-E5071SetPower(ENA,-30); % in dBm
+E5071SetPower(ENA,-20); % in dBm
 E5071SetNumPoints(ENA,4000); % set no. of points
 E5071SetDataFormat(ENA,'PLOG'); % SLIN, SLOG, SCOM, SMIT, SADM, PLIN, PLOG, or POL
 E5071SetDelay(ENA,500,4000); % correct for electrical delay
 E5071SetIFBand(ENA,1); % in kHz
 E5071SetSweepTime(ENA,4); % in secs
-
-% fopen(ENA) % open client connection
-% fclose(ENA) % close client connection, or: clear ENA
-% query(ENA,'*IDN?') % check connection to ENA
