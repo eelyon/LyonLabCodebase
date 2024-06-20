036947c08822cad1f23398c4b32ea65948c4e574
matt <fizixboi@gmail.com>
2024-06-20 18:12:38 -0400

    Update setVoltagesDAC.m

--- matlabMeas/MattMeasurementFunctions/resThermo/oldStuff/setVoltagesDAC.m
+++ matlabMeas/MattMeasurementFunctions/resThermo/oldStuff/setVoltagesDAC.m
@@ -3,7 +3,7 @@
 channels = [1, 2, 3, 4, 5, 6, 7, 8];
 numSteps = 100;
 
-voltsEmittMeas = [-1.0,...  %DAC 1
+voltsEmittMeas = [0.3,...  %DAC 1
         -0.3,...   %DAC 2
         -0.0,...   %DAC 3
         -0.0,...   %DAC 4
