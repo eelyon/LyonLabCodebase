0a25058e6bffcdea6eb9a7a7d661465b61ba67a1
gordianf1 <gordian.fuchs@gmail.com>
2024-08-18 19:22:13 -0400

    plotAnsysData

---
+++ matlabMeas/GordianMeasurementFunctions/DataAnalysis/plotANSYS.m
@@ -0,0 +1,31 @@
+data = readtable('C:\Users\Lyon Lab Simulation\Documents\Ansoft\simulation files (new)\Sandia23\voltage2D.txt','HeaderLines',2);
+x = data.Var1;
+y = data.Var2;
+voltage = data.Var4;
+xVal = 0:0.1e-6:25e-6;
+yVal = 0:0.1e-6:3e-6;
+spacing = 0.1e-6;
+
+vData = [];
+
+for i = 1:length(xVal)
+    for j = 1:length(yVal)
+        vData(i,j) = voltage(length(yVal)*(i-1)+j);
+    end
+end
+
+figure
+% plot(yVal(1:30),voltage(1:30))
+% 
+% pcolor(xVal*1e6,yVal*1e6,vData.');
+% shading interp
+% % title("contour plot")
+% xlabel('x (\mum)')
+% ylabel('y (\mum)')
+
+image(x,y,voltage.','CDataMapping','scaled')
+set(gca,'YDir','normal')
+
+% h = colorbar;
+% h.Label.VerticalAlignment = "bottom";
+% ylabel(h,'V (x,y)','FontSize',12,'Rotation',270)
