1d6fd05db182669ff52f05fb729aa64367ef3d29
爱门 <34817875+MissPinkElfLady@users.noreply.github.com>
2024-10-18 16:38:40 -0400

    added sdg5122 support for cap compensation

--- matlabMeas/Initialization/initialize_Experiment_WF.m
+++ matlabMeas/Initialization/initialize_Experiment_WF.m
@@ -5,6 +5,9 @@ port = 1234;
 % SR830
 SR830_Address = '172.29.117.103';
 SR830 = SR830(port,SR830_Address);
+% sweep1DMeasSR830Fast({'SM'},
+% first argument only changes the axis names
+
 
 % DAC
 sigDACPort = 'COM4';
@@ -18,6 +21,11 @@ comPort = 'COM5';
 SIM900 = SIM900(comPort);
 % simply call functions in SIM900, e.g. querySIM900Voltage(SIM900,7)
 % rampSIM900Voltage(SIM900,port,voltage, pauser, delta)
+
+% SDG5122
+% connect to SDG5122 via USB-B
+SDGPort = 'COM';
+SDG5122 = SDG5122(SDGPort);
 
 % If needed run setupENA in folder as well.
 % connect ethernet cable behind to VNA
