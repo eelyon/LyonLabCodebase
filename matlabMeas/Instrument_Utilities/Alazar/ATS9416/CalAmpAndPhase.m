f063baa0d1f63052f4f60cb4e464fc141fbcb118
gordianf1 <gordian.fuchs@gmail.com>
2024-12-11 22:45:06 -0500

    Alazar_2024/12/11_3

--- matlabMeas/Instrument_Utilities/Alazar/ATS9416/CalAmpAndPhase.m
+++ matlabMeas/Instrument_Utilities/Alazar/ATS9416/CalAmpAndPhase.m
@@ -1,19 +1,26 @@
 % Parameters
-Fs = 100e6;          % Sampling frequency (Hz)
-postTriggerSamples = 100000;
-T = postTriggerSamples/Fs; % 1;               % Duration of the signal (seconds)
-t = 0:1/Fs:T-1/Fs;   % Time vector
-N = length(t);
-f = (-N/2:N/2-1)*(Fs/N);  % Frequency vector
-f_signal = 1e6;       % Frequency of the signal (Hz)
+Fs = 100e6; % Sampling frequency (Hz)
+postTriggerSamples = 1000;
+T = postTriggerSamples/Fs; % Duration of the signal (seconds)
+t = 0:1/Fs:T-1/Fs; % Time vector
+f_signal = 1e6; % Frequency of the signal (Hz)
 signal_amplitude = 1;
-noise_level = 0;   % Amplitude of noise
+noise_level = 0.1; % Amplitude of noise
 
-signal = signal_amplitude * cos(2 * pi * f_signal * t + 45*pi/180) + noise_level * randn(size(t));
-sinFit = fit(t, signal, 'sin1')
+y = signal_amplitude * cos(2 * pi * f_signal * t + 23*pi/180) + noise_level * randn(size(t));
+yf = lowpass(y, 1e6, Fs);
+
+amp = (max(yf) - min(yf))/2;
+phase = 20;
+dcOffset = mean(yf);
+ 
+fit = @(b,t)  b(1).*(cos(2*pi*t*f_signal + b(2)*pi/180)) + b(3); % Function to fit
+fcn = @(b) sum((fit(b,t) - yf).^2); % Least-Squares cost function
+s = fminsearch(fcn, [amp; phase; dcOffset]) % Minimise Least-Squares
 
 figure
-plot(t(1:100),signal(1:100))
+plot(t,y)
 hold on
-plot(t(1:100),sinFit(1:100))
+plot(t,fit(s,t))
 hold off
+legend('signal','fit')
