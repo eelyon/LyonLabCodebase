75b4b43cbd44745be76d23d8391dcb8c7df6d168
matt <fizixboi@gmail.com>
2025-05-12 19:37:09 -0400

    Update nstFreqSweeps_v01.m

--- matlabMeas/MattMeasurementFunctions/NST/nstFreqSweeps_v01.m
+++ matlabMeas/MattMeasurementFunctions/NST/nstFreqSweeps_v01.m
@@ -1,3 +1,3 @@
-sweep1DMeasSR830Fast({'Freq'},1,9.5,0.5,5,20,{SR830},SDG5122,{'DualFreq'},0);
-sweep1DMeasSR830Fast({'Freq'},10,95,5,5,20,{SR830},SDG5122,{'DualFreq'},0);
-sweep1DMeasSR830Fast({'Freq'},100,1000,50,5,20,{SR830},SDG5122,{'DualFreq'},0);
+sweep1DMeasSR830Fast({'Freq'},1,9.5,0.5,5,20,{SR830},SR830,{'DualFreq'},0);
+sweep1DMeasSR830Fast({'Freq'},10,95,5,5,20,{SR830},SR830,{'DualFreq'},0);
+sweep1DMeasSR830Fast({'Freq'},100,1000,50,5,20,{SR830},SR830,{'DualFreq'},0);
