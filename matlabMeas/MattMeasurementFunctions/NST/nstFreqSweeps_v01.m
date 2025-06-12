380b06c98b04fee8364e453436a02295141e8637
matt <fizixboi@gmail.com>
2025-06-12 18:08:36 -0400

    cooldown done

--- matlabMeas/MattMeasurementFunctions/NST/nstFreqSweeps_v01.m
+++ matlabMeas/MattMeasurementFunctions/NST/nstFreqSweeps_v01.m
@@ -1,3 +1,5 @@
 sweep1DMeasSR830Fast({'Freq'},1,10,0.5,5,20,{SR830},SR830,{'DualFreq'},0);
 sweep1DMeasSR830Fast({'Freq'},12,107,5,5,20,{SR830},SR830,{'DualFreq'},0);
 sweep1DMeasSR830Fast({'Freq'},117,1017,50,5,20,{SR830},SR830,{'DualFreq'},0);
+%sweep1DMeasSR830Fast({'Freq'},1270,10270,500,5,20,{SR830},SR830,{'DualFreq'},0);
+%sweep1DMeasSR830Fast({'Freq'},15270,100270,5000,5,20,{SR830},SR830,{'DualFreq'},0);
