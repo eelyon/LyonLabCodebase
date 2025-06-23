2d5f5a162e7ce23526258f23d2df98f9799fd31e
mayer <118573753+tiffliu9@users.noreply.github.com>
2025-06-23 16:59:23 -0400

    Update temperatureVsTime_Script.m

--- matlabMeas/Thermometry/temperatureVsTime_Script.m
+++ matlabMeas/Thermometry/temperatureVsTime_Script.m
@@ -1,10 +1,15 @@
 %% Frequency of temperature querying in seconds.
-timeBetweenPoints = 10;
+timeBetweenPoints = 3;
 
 %% Initialize Thermometer type (this is related to your thermometer you have)
 
+<<<<<<< Updated upstream
 %thermometerType = 'X117656'; %Big Glass Dewar
 thermometerType = 'X189328'; %Small Glass Dewar
+=======
+thermometerType = 'X117656'; %Big Glass Dewar
+%thermometerType = 'X189328'; %Small Glass Dewar
+>>>>>>> Stashed changes
 %thermometerType = 'X204446'; %Dunking Thermometer
 %thermometerType = 'X189327'; %CIA Stick
 
