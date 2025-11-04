6ea803f3ac1fb30cd8cac065d3940c26ef5fdb81
gordianf1 <gordian.fuchs@gmail.com>
2025-11-04 14:30:13 -0500

    plotting_20251104

--- matlabMeas/GordianMeasurementFunctions/ccdPaperDataAnalysis/MatlabPlotting.m
+++ matlabMeas/GordianMeasurementFunctions/ccdPaperDataAnalysis/MatlabPlotting.m
@@ -1,16 +1,16 @@
-whosePath = 'B124';
+whosePath = 'B417';
 tag = 'Guard1';
 
 % numEs = [];
 % yErrs = [];
 % Vloads = 1:-0.05:0; % [-0.1 0 0.1 0.2 0.3 0.4];
 
-gain = 24.3*0.915; % Enter gain from roll-off plot
-cap = 3.55e-12; % Enter approximate HEMT input capacitance
+gain = 24*0.92; % Enter gain from roll-off plot
+cap = 3.16e-12; % Enter approximate HEMT input capacitance
 
 switch whosePath 
     case 'B417'
-        path = 'C:\Users\Lyon Lab Simulation\Princeton Dropbox\Gordian Fuchs\GroupDropbox\Gordian\Experiments\Sandia2023\single_electron_sensitivity\data_single_electron_shuttling\10_15_25\';
+        path = 'C:\Users\Lyon Lab Simulation\Princeton Dropbox\Gordian Fuchs\GroupDropbox\Gordian\Experiments\Sandia2023\SingleElectronSensingShuttling\data_single_electron_shuttling\11_03_25\';
     case 'B124'
         path = 'C:\Users\Lyon-Lab-B417\Documents\GitHub\LyonLabCodebase\matlabMeas\Data\11_04_25\';
     case 'gordian'
@@ -20,27 +20,31 @@ switch whosePath
 end
 
 % Try and catch errors in loop
-figPath1 = append(path,tag,'_',num2str(21488),'.fig');
-% figPath2 = append(path,tag,'_',num2str(20835),'.fig');
-% figPath3 = append(path,tag,'_',num2str(20836),'.fig');
-% figPath4 = append(path,tag,'_',num2str(20837),'.fig');
-% figPath5 = append(path,tag,'_',num2str(20838),'.fig');
-% figPath6 = append(path,tag,'_',num2str(20839),'.fig');
+% figPath1 = append(path,tag,'_',num2str(21417),'.fig');
+figPath2 = append(path,tag,'_',num2str(21421),'.fig');
+figPath3 = append(path,tag,'_',num2str(21422),'.fig');
+% figPath4 = append(path,tag,'_',num2str(21423),'.fig');
+figPath8 = append(path,tag,'_',num2str(21425),'.fig');
+figPath5 = append(path,tag,'_',num2str(21426),'.fig');
+figPath6 = append(path,tag,'_',num2str(21428),'.fig');
+figPath7 = append(path,tag,'_',num2str(21429),'.fig');
 
 figure()
-plot_electrons(figPath1,1,2,cap,gain,1)
-% plot_electrons(figPath2,1,2,cap,gain,0.2)
-% plot_electrons(figPath3,1,2,cap,gain,0.1)
-% plot_electrons(figPath4,1,2,cap,gain,0.05)
-% plot_electrons(figPath5,1,2,cap,gain,0.01)
-% plot_electrons(figPath6,1,2,cap,gain,0.005)
+% plot_electrons(figPath1,1,2,cap,gain,-0.1)
+plot_electrons(figPath2,1,2,cap,gain,-0.18)
+plot_electrons(figPath3,1,2,cap,gain,-0.2)
+% plot_electrons(figPath4,1,2,cap,gain,-0.22)
+plot_electrons(figPath8,1,2,cap,gain,-0.26)
+plot_electrons(figPath5,1,2,cap,gain,-0.28)
+plot_electrons(figPath6,1,2,cap,gain,-0.32)
+plot_electrons(figPath7,1,2,cap,gain,-0.34)
 
 hold off
 
 set(gca,'FontSize',13)
 xlabel('V_{guard} (V)','FontSize',14)
 ylabel('V_{rms} (\mu V)','FontSize',14)
-% xlim([-0.6,0.3])
+xlim([-0.8,0.1])
 % ylim([0,0.6])
 legend;
 
@@ -61,8 +65,8 @@ function plot_electrons(figPath,ax1,ax2,cap,gain,Vload)
     close(fig);
     
     mag = correctedMag(avgReal,avgImag); % Get corrected magnitude
-    delta = max(mag) - min(mag) % mag(3)-mag(end-5); %  Calc. change in signal
-    numE = calcNumElectrons(cap,delta,gain) % Calc. tot. no. of electrons
+    delta = max(mag) - min(mag); % mag(3)-mag(end-5); %  Calc. change in signal
+    numE = calcNumElectrons(cap,delta,gain); % Calc. tot. no. of electrons
     
     stdm = sqrt(stdReal.^2 + stdImag.^2); % Calc. standard deviation of magnitude
     deltaErr = stdm(find(mag==max(mag)))+stdm(find(mag==min(mag))); % stdm(3)+stdm(end-5); 
@@ -70,7 +74,7 @@ function plot_electrons(figPath,ax1,ax2,cap,gain,Vload)
     
     display(numE);
     display(yErr);
-    errorbar(Vshield,mag*1e6,stdm*1e6,'.-','MarkerSize',14,'DisplayName',['n=',num2str(round(numE),'%d'),' (V_{Dr1,Dr2}=',num2str(Vload),'V)']) % ' (tc=',num2str(Vload),'s)'])
+    errorbar(Vshield,mag*1e6,stdm*1e6,'.-','MarkerSize',14,'DisplayName',['n=',num2str(round(numE,1)),' (V_{d1,d2} = ',num2str(Vload),' V)']) % ' (tc=',num2str(Vload),'s)'])
 %     ['n = ',num2str(numE,'%.1f'),' ',177, ' ', num2str(yErr,'%.1f')]
 %     legend('n = %i', numE);
     hold on
