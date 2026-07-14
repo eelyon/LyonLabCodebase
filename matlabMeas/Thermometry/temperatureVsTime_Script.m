f97de939212d1beb0bc34c175f0f19e4f71ae9f9
matt <fizixboi@gmail.com>
2026-07-14 17:45:32 -0400

    hgh

--- matlabMeas/Thermometry/temperatureVsTime_Script.m
+++ matlabMeas/Thermometry/temperatureVsTime_Script.m
@@ -6,8 +6,8 @@ timeBetweenPoints = 5;
 %thermometerType = 'X117656'; %Big Glass Dewar
 %thermometerType = 'X189328'; %Small Glass Dewar
 %thermometerType = 'X204446'; %Dunking Thermometer
-thermometerType = 'X189327'; %CIA Stick
-%thermometerType = 'X105783'; %Kelvin Probe
+%thermometerType = 'X189327'; %CIA Stick
+thermometerType = 'X105783'; %Kelvin Probe
 
 Thermometer;
 Therm = initializeThermometry(thermometerType);
